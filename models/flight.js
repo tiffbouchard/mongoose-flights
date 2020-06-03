388c4dc8d7584c5ae1fa87e4ff124d987be0e849
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-03 17:51:19 -0400

    Define flight schema

---
+++ models/flight.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+  airline: String, 
+  airport: String,
+  flightNo: Number, 
+  departs: Date, 
+});
