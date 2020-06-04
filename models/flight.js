5f675c27e5ceed14fd5c37d1084de2ce7ff8af0f
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-03 21:46:47 -0400

    Add some default values to flight schema

--- models/flight.js
+++ models/flight.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-  airline: String, 
-  airport: String,
-  flightNo: Number, 
-  departs: Date, 
+  airline: String, //validation of American, Southwest and United
+  airport: { type: String, default: "DEN" }, //default value to DEN validation to enum AUS DFW DEN LAX SAN
+  flightNo: Number, //validation required between 10 and 9999
+  departs: { type: Date, 
+    default: function() {
+      return // year created  + 1
+    }
+  }, //how to return the year plus one
 });
