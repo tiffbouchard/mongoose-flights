1d4b844449ebf2fca019d5faaf33ed830992a2f5
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-09 23:29:20 -0400

    Update flightsRouter

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var flightsRouter = require('./routes/flights');
+
 
 require('./config/database');
 
@@ -22,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/flights', flightsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
