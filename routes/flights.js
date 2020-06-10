09b6ba704139dec16114c89e1f83bde102fa6c07
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-09 23:30:20 -0400

    Add index and new flight controllers

---
+++ routes/flights.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const flightsCtrl = require('../controllers/flights')
+
+router.get('/', flightsCtrl.index);
+router.get('/new', flightsCtrl.new);
+
+
+module.exports = router;
