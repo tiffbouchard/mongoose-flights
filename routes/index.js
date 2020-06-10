fcfe3931f0a3509d11b5c7a34fa40a1dd652ed5e
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-09 22:32:22 -0400

    Add bootstrap change views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/flights');
+  res.render('index', { title: 'Mongoose Flights' });
 });
 
 module.exports = router;
