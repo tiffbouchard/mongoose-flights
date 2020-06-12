3654a3f9b0f3fa5d01a215865f68d3ff87f43065
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-11 23:05:26 -0400

    Add show route

--- routes/flights.js
+++ routes/flights.js
@@ -5,5 +5,6 @@ const flightsCtrl = require("../controllers/flights");
 router.get("/", flightsCtrl.flightsIndex);
 router.get("/new", flightsCtrl.new);
 router.post("/", flightsCtrl.createFlight);
+router.get("/:id", flightsCtrl.show);
 
 module.exports = router;
