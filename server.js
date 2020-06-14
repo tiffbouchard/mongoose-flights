cbafc495f394fe18ce3c61b848b5be7c7761556b
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-06-13 21:21:11 -0400

    Add tickets router

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ require("./config/database");
 var indexRouter = require("./routes/index");
 var flightsRouter = require("./routes/flights");
 const destinationsRouter = require("./routes/destinations");
+var ticketRouter = require("./routes/tickets");
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/flights", flightsRouter);
 app.use("/", destinationsRouter);
+app.use("/tickets", ticketRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
